5c3b60629a2b2ce7583f0400b292d9023257a44e
Chris Connell <chris.g.connell@gmail.com>
2016-07-06 17:18:04 -0500

    working on styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def about
+  end
+
+  def mini
+  end
+
+  def projects
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'pages#index'
+  get '/about' => 'pages#about'
+  get '/projects' => 'pages#projects'
+  get '/minimalist' => 'pages#mini'
 end
